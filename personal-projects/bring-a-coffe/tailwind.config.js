99ee467b8a94763deb5b0b0e3df35f269c61b8e2
Victor Yuri <victoryuritc@yahoo.com>
2022-08-12 21:29:07 -0300

    changing heart svg animation and size

--- personal-projects/bring-a-coffe/tailwind.config.js
+++ personal-projects/bring-a-coffe/tailwind.config.js
@@ -16,14 +16,17 @@ module.exports = {
       'espresso': '#ede0d4'
       },
       animation: {
-        wiggle: 'wiggle 1s ease-in-out infinite',
+        wiggle: 'wiggle 3s ease-in-out infinite',
       },
       keyframes: {
         wiggle: {
-          '0%, 100%': { transform: 'rotate(-12deg)'},
-            '50%': { transform: 'rotate(12deg)'},
-        }
-      }
+          '0%, 100%': { transform: 'translate(0px, 0.75px)' },
+          '20%': { transform: 'rotate(12deg) translate(0px, 0px)' },
+          '40%': { transform: 'rotate(-12deg)' },
+          '60%': { transform: 'rotate(6deg)' },
+          '80%': { transform: 'rotate(-6deg)' },
+        },
+      },
     },
   },
   plugins: [],
