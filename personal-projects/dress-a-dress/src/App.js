90a94ebdc43f3cd695c3140b0c292ea3f7753b8f
Victor Yuri <victoryuritc@yahoo.com>
2022-09-03 20:21:04 -0300

    initial commit with gabi

---
+++ personal-projects/dress-a-dress/src/App.js
@@ -0,0 +1,15 @@
+
+
+function App() {
+  return (
+    <div className="bg-red-500 flex flex-col items-center justify-center md:flex-row gap-5">
+      <div className="bg-green-500">
+        A
+      </div>
+      <div className="bg-blue-500">B</div>
+      <div className="bg-violet-500">C</div>
+    </div>
+  );
+}
+
+export default App;
