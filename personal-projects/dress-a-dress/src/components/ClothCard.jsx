a74754388cfe43c954273f756ca31d30078b1281
Victor Yuri <victoryuritc@yahoo.com>
2022-09-04 20:48:10 -0300

    swatchCard and available colors

--- personal-projects/dress-a-dress/src/components/ClothCard.jsx
+++ personal-projects/dress-a-dress/src/components/ClothCard.jsx
@@ -1,10 +1,16 @@
-import React, { useCallback, useState } from 'react'
+import React, { useState } from 'react'
 import HeartSVG from '../assets/images/svgs/miscellaneous/svgs';
+import SwatchCard from './SwatchCard';
 
 function ClothCard({ cloth }) {
   const [imageSrc, setImageSrc] = useState(cloth.image[0].src)
 
-  const { price, title } = cloth;
+  const {
+    price,
+    title,
+    sellingAttribute,
+    swatches
+  } = cloth;
 
   const onMouseEnterChangeImageSrc = () => {
     setImageSrc(cloth.image[0].dataAltImage)
@@ -23,13 +29,21 @@ function ClothCard({ cloth }) {
           alt={ cloth.image[0].alt }
           onMouseEnter={ onMouseEnterChangeImageSrc }
           onMouseLeave={ onMouseLeaveChangeImageSrc }
+          className="group-hover:cursor-pointer focus:opacity-20 transition"
         />
         <span className="">
-          <HeartSVG className="transition duration-200 group-hover:cursor-pointer -translate-y-7 translate-x-distant hover:fill-red-400"/>
+          <HeartSVG className="transition duration-200 group-hover:cursor-pointer -translate-y-7 translate-x-distant fill-white hover:fill-red-300"/>
         </span>
       </div>
-      <span className="p-3 border rounded m-3 text-center w-full h-full">{ title }</span>
-      <span>{ price }</span>
+      <span className="self-start text-start text-sm">{ title }</span>
+      <span className="self-start text-start text-sm">{ price }</span>
+      <span className="self-start flex flex-row gap-1" >{ swatches.map(swatch => (
+        <SwatchCard
+          swatch={ swatch }
+          key={ `${cloth.articleCode}-${swatch.colorCode}` }
+        />
+      )) }</span>
+      <span className="self-start text-start text-xs">{ sellingAttribute }</span>
     </div>
   );
 }
