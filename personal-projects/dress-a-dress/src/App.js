b87739b0a0dfa0aabaf14a8c7a0299ee43ddea35
Victor Yuri <victoryuritc@yahoo.com>
2022-09-04 02:30:22 -0300

    heart svg component

--- personal-projects/dress-a-dress/src/App.js
+++ personal-projects/dress-a-dress/src/App.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { HEART_ICON } from "./assets/images/svgs/miscellaneous/svgs";
+import HeartSVG, { HEART_ICON } from "./assets/images/svgs/miscellaneous/svgs";
 import { getDataFromAPI } from "./services/fashionAPI";
 import fashionData from "./services/fashionData";
 
@@ -7,18 +7,19 @@ function App() {
   return (
     <div className="grid grid-cols-5">
       { fashionData && (
-        fashionData.map(({ title, image }) => (
+        fashionData.map(({ title, image, price }) => (
           <div className="cloth-card flex flex-col items-center justify-center border-2 p-3">
             <div className="">
               <img
                 src={ image[0].dataAltImage }
                 alt={ image[0].alt }
               />
-              <span className="absolute -translate-y-7 translate-x-distant">
-                { HEART_ICON }
+              <span className="">
+                <HeartSVG className="transition duration-10000 hover:fill-red-400 hover:cursor-pointer absolute -translate-y-7 translate-x-distant hover:fill-red-100"/>
               </span>
             </div>
             <span className="p-3 border rounded m-3 text-center w-full h-full">{ title }</span>
+            <span>{ price }</span>
           </div>
         ))
       )}
--- personal-projects/dress-a-dress/tailwind.config.js
+++ personal-projects/dress-a-dress/tailwind.config.js
@@ -6,7 +6,10 @@ module.exports = {
   theme: {
     extend: {
       translate: {
-        'distant': '12rem'
+        'distant': '12.25rem'
+      },
+      transitionDuration: {
+        '10000': '10000ms'
       }
     },
   },
