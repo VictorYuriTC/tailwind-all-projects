a01717e7948e1da26571519b9f354866f02f6d14
Victor Yuri <victoryuritc@yahoo.com>
2022-09-05 04:55:44 -0300

    font, display and position adjust at ClothCard

--- personal-projects/dress-a-dress/src/components/ClothCard.jsx
+++ personal-projects/dress-a-dress/src/components/ClothCard.jsx
@@ -6,6 +6,7 @@ function ClothCard({ cloth }) {
   const [imageSrc, setImageSrc] = useState(cloth.image[0].src)
 
   const {
+    marketingMarkerText,
     price,
     title,
     sellingAttribute,
@@ -22,35 +23,35 @@ function ClothCard({ cloth }) {
 
 
   return (
-    <div className="cloth-card flex flex-col items-center justify-center p-1">
+    <div className="cloth-card grid p-1 w-fit">
       <div className="group">
         <img
           src={ imageSrc }
           alt={ cloth.image[0].alt }
           onMouseEnter={ onMouseEnterChangeImageSrc }
           onMouseLeave={ onMouseLeaveChangeImageSrc }
-          className="group-hover:cursor-pointer focus:opacity-20 transition"
+          className="group-hover:cursor-pointer focus:opacity-20 transition w-fit"
         />
         <AddToFavoriteHeartSVG
-          className="transition duration-200 group-hover:cursor-pointer -translate-y-7 translate-x-distant hover:fill-red-300"
+          className="absolute self-end items-end transition duration-200 group-hover:cursor-pointer hover:fill-red-300 -translate-y-6 translate-x-distant"
           articleCode={ cloth.articleCode }
         />
       </div>
-
-      <span className="self-start text-start text-sm">{ title }</span>
-
-      <span className="self-start text-start text-sm">{ price }</span>
-
-      <span className="self-start flex flex-row gap-1" >
-        { swatches.map(swatch => (
-          <SwatchCard
-            swatch={ swatch }
-            key={ `${cloth.articleCode}-${swatch.colorCode}` }
-          />))
-        }
-      </span>
-
-      <span className="self-start text-start text-xs">{ sellingAttribute }</span>
+  
+      <div className="flex flex-col w-fit">
+        <span className="self-start text-start font-medium text-sm text-[#5f5f5f]">{ marketingMarkerText }</span>
+        <span className="self-start text-start text-sm">{ title }</span>
+        <span className="self-start text-start text-sm">{ price }</span>
+        <span className="self-start flex flex-row gap-1" >
+          { swatches.map(swatch => (
+            <SwatchCard
+              swatch={ swatch }
+              key={ `${cloth.articleCode}-${swatch.colorCode}` }
+            />))
+          }
+        </span>
+        <span className="self-start text-start text-xs">{ sellingAttribute }</span>
+      </div>
 
     </div>
   );
