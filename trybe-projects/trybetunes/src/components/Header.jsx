5556cae893712bb01980277be25acb1000e36511
Victor Yuri <victoryuritc@yahoo.com>
2022-09-11 20:30:41 -0300

    SearchPage started. his-color extended

--- trybe-projects/trybetunes/src/components/Header.jsx
+++ trybe-projects/trybetunes/src/components/Header.jsx
@@ -3,7 +3,7 @@ import EarbudSVG from './svgs/Earbud.SVG';
 
 function Header(props) {
   return (
-    <section className="flex flex-row items-center justify-center gap-6 p-1">
+    <section className="flex flex-row items-center justify-center gap-6 p-1 hover:scale-105 transition duration-500 hover:cursor-pointer">
       <span>
         { <EarbudSVG className="fill-[#400090] w-16 h-16"/> }
       </span>
