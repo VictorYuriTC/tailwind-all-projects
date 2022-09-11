40e9ac55a1b244f5bad8fcb950b8f7a3cb1440b6
Victor Yuri <victoryuritc@yahoo.com>
2022-09-11 17:39:49 -0300

    tailwind react-router-dom Login NotFound pages added

---
+++ trybe-projects/trybetunes/src/pages/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function LoginPage(props) {
+  return (
+    <div className='flex items-center justify-center min-h-screen'>
+      <span className="text-2xl">
+        Login
+      </span>
+    </div>
+  );
+}
+export default LoginPage;
