cd845b547a7ea6e9036cd3940790f973b8b3cf3a
Victor Yuri <victoryuritc@yahoo.com>
2022-09-11 17:58:03 -0300

    add Login inputs

--- trybe-projects/trybetunes/src/pages/Login.jsx
+++ trybe-projects/trybetunes/src/pages/Login.jsx
@@ -2,10 +2,33 @@ import React from 'react'
 
 function LoginPage(props) {
   return (
-    <div className='flex items-center justify-center min-h-screen'>
-      <span className="text-2xl">
-        Login
-      </span>
+    <div className="flex flex-col gap-2 items-center justify-center min-h-screen bg-[#400090]">
+      <label
+        htmlFor=""
+        className="flex gap-2 "
+      >
+        <span className="text-white text-xl font-medium">
+          Login:
+        </span>
+        <input
+          type="text"
+          className="rounded-lg"
+        />
+      </label>
+
+      <label
+        htmlFor="" className="flex gap-2 ">
+        <span className="text-white text-xl font-medium">
+          Password:
+        </span>
+        <input
+          type="password"
+          className="rounded-lg"
+        />
+      </label>
+      <button type="password">
+
+      </button>
     </div>
   );
 }
