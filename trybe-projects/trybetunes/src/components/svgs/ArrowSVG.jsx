429a53318f5c4dac91459a421701d057fc665f1a
Victor Yuri <victoryuritc@yahoo.com>
2022-09-15 01:50:58 -0300

    LoginPage concluded

--- trybe-projects/trybetunes/src/components/svgs/ArrowSVG.jsx
+++ trybe-projects/trybetunes/src/components/svgs/ArrowSVG.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 
-function ArrowSVG(props) {
+function ArrowSVG({ className }) {
   return (
     <svg
       xmlns="https://wwww.org/2000.svg"
-      class="w-7"
+      className={ className }
       viewBox="0 0 24 24"
-      stroke-width="1.5"
+      strokeWidth="1.5"
       stroke="#FFFFFF"
       fill=""
-      stroke-linecap="round"
-      stroke-linejoin="round">
+      strokeLinecap="round"
+      strokeLinejoin="round">
       <path
         stroke="none"
         d="M0 0h24v24H0z"
--- trybe-projects/trybetunes/src/pages/LoginPage.jsx
+++ trybe-projects/trybetunes/src/pages/LoginPage.jsx
@@ -1,13 +1,58 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 import Header from '../components/menus/Header';
 import ArrowSVG from '../components/svgs/ArrowSVG';
 import UserSVG from '../components/svgs/UserSVG'
 import GOOGLE_PNG from '../assets/images/google.png'
 import FACEBOOK_PNG from '../assets/images/facebook.png'
+import { ENTER } from '../constants/strings';
 
 function LoginPage(props) {
+  const [isLoginButtonDisabled, setIsLoginButtonDisabled] = useState(true);
+  const [usernameInput, setUsernameInput] = useState('');
+  const [passwordInput, setPasswordInput] = useState('');
+  const [loginButtonStyle, setLoginButtonStyle] = useState(
+    { 
+      boxShadow: '',
+      opacity: 1,
+      translate: '0',
+    })
+
   const navigate = useNavigate();
+
+  useEffect(() => {
+    const usernameLength = usernameInput.length;
+    const passwordLength = passwordInput.length;
+
+    const hasUsernameMinChar = usernameLength >= 3 ? true : false
+    const hasPasswordMinChar = passwordLength >= 6 ? true : false
+
+    if (hasUsernameMinChar && hasPasswordMinChar) { 
+      setIsLoginButtonDisabled(false)
+      return;
+    }
+
+    setIsLoginButtonDisabled(true)
+  }, [usernameInput, passwordInput])
+
+  const onEnterKeyDownNavigateToSearchPage = ({ key }) => {
+    if (key !== ENTER) return;
+
+    if (!isLoginButtonDisabled) navigate('/search')
+    
+
+    if (isLoginButtonDisabled) {
+      alert('Only usernames with 3 or more characters and passwords with 6 or more characters are valid.')
+    }
+  }
+
+  const handleUsernameInputChange = ({ target: { value }}) => {
+    setUsernameInput(value)
+  }
+
+  const handlePasswordInputChange = ({ key, target: { value }}) => {
+    setPasswordInput(value)
+  }
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-[#efefef]">
       <div className="absolute top-0 w-full mb-20">
@@ -22,24 +67,40 @@ function LoginPage(props) {
           </span>
         </div>
         <div>
-          <p class="text-black max-w-sm font-sans font-light mb-7">Login to your account to listen the newest songs in the Trybe industry.
+          <p className="text-black max-w-sm font-sans font-light mb-7">Login to your account to listen the newest songs in the Trybe industry.
           </p>
         </div>
         <div className="flex flex-col mb-4 md:mb-10 space-y-2">
           <label htmlFor="" className="flex flex-row">
-            <input type="text" placeholder="Username" className="w-fit p-3 border rounded-lg placeholder:text-black placeholder:font-thin focus:outline-her-green"/>
+            <input
+              value={ usernameInput }
+              onChange={ handleUsernameInputChange }
+              onKeyDown={ onEnterKeyDownNavigateToSearchPage }
+              type="text"
+              placeholder="Username"
+              className="w-fit p-3 border rounded-lg placeholder:text-black placeholder:font-thin focus:outline-her-green"/>
           </label>
           <label htmlFor="" className="flex flex-row">
-            <input type="password" placeholder="Password" className="w-fit p-3 border rounded-lg placeholder:text-black placeholder:font-thin focus:outline-her-green"/>
+            <input
+              value={ passwordInput }
+              onChange={ handlePasswordInputChange }
+              onKeyDown={ onEnterKeyDownNavigateToSearchPage }
+              type="password"
+              placeholder="Password"
+              className="w-fit p-3 border rounded-lg placeholder:text-black placeholder:font-thin focus:outline-her-green"/>
           </label>
         </div>
 
-        <div class="flex flex-col items-center justify-between space-x-0 space-y-6 
+        <div className="flex flex-col items-center justify-between space-x-0 space-y-6 
           md:flex-row md:space-y-0 md:space-x-12 mb-6 md:mb-12"
         >
           <button className="font-extralight text-black border-b py-2">Forgot password</button>
 
-          <button class="text-white w-full md:w-auto flex justify-center items-center space-x-1 font-sans font-semibold rounded-md shadow-lg px-3 py-2 hover:bg-opacity-90 hover:shadow-lg border transition duration-200 hover:-translate-y-[1px] bg-his-purple"
+          <button
+            onClick={ () => navigate('/search') }
+            disabled={ isLoginButtonDisabled }
+            className="text-white w-full md:w-auto flex justify-center items-center space-x-1 font-sans font-semibold rounded-md shadow-lg px-3 py-2 border transition duration-200 bg-his-purple"
+            style={ loginButtonStyle }
           >
             <span>
               Login
@@ -50,12 +111,14 @@ function LoginPage(props) {
 
         
         <div>
-          <div class="border-b border-gray-300"></div>
+          <div className="border-b border-gray-300"></div>
 
-          <p class="py-6 text-sm font-extralight text-center text-gray-900">or login with</p>
+          <p className="py-6 text-sm font-extralight text-center text-gray-900">or login with</p>
 
           <div className="flex flex-col space-x-0 space-y-5 md:flex-row md:space-y-0 md:space-x-5">
-            <button class="flex items-center justify-center py-2 px-5 space-x-3 border border-gray-300 rounded shadow-sm hover:bg-opacity-30 hover:shadow-lg hover:-translate-y-[1px] transition duration-200 md:w-1/2">
+            <button
+              onClick={() => navigate('/')}
+              className="flex items-center justify-center py-2 px-5 space-x-3 border border-gray-300 rounded shadow-sm hover:bg-opacity-30 hover:shadow-lg hover:-translate-y-[1px] transition duration-200 md:w-1/2">
               <img
                 src={ FACEBOOK_PNG }
                 alt="Logo"
@@ -64,7 +127,9 @@ function LoginPage(props) {
                 Facebook
               </span>
             </button>
-            <button class="flex items-center justify-center py-2 px-2 space-x-3 border border-gray-300 rounded shadow-sm hover:bg-opacity-30 hover:shadow-lg hover:-translate-y-[1px] transition duration-200 md:w-1/2">
+            <button
+              onClick={() => navigate('/')}
+              className="flex items-center justify-center py-2 px-2 space-x-3 border border-gray-300 rounded shadow-sm hover:bg-opacity-30 hover:shadow-lg hover:-translate-y-[1px] transition duration-200 md:w-1/2">
               <img
                 src={ GOOGLE_PNG }
                 alt="Logo"
