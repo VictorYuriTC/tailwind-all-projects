94ecea24af08dc4a2f979ab1aa16ff9a7fa48f43
Victor Yuri <victoryuritc@yahoo.com>
2022-09-16 20:43:40 -0300

    HamburguerMenu when sm screen and refactoring

--- trybe-projects/trybetunes/src/components/menus/SearchAsideBarLinks.jsx
+++ trybe-projects/trybetunes/src/components/menus/SearchAsideBarLinks.jsx
@@ -5,8 +5,9 @@ import UserSVG from '../svgs/UserSVG';
 import StarSVG from '../svgs/StarSVG';
 import LogoutSVG from '../svgs/LogoutSVG';
 import SongsContext from '../../context/SongsContext';
-import { ENTER } from '../../constants/strings';
+import { ENTER, FLEX, NONE } from '../../constants/strings';
 import MagnifyingGlassSVG from '../svgs/MagnifyingGlassSVG';
+import HamburguerSVG from '../svgs/HamburguerSVG';
 
 function SearchAsideBarLinks(props) {
   const contextValue = useContext(SongsContext);
@@ -20,19 +21,27 @@ function SearchAsideBarLinks(props) {
   const [isSearchedArtistInputVisible, setIsSearchedArtistInputVisible] = useState(true);
   const [isSearchMessageVisible, setIsSearchMessageVisible] = useState(false);
   const [windowSize, setWindowSize] = useState(window.innerWidth);
+  const [isDisplayMenuLinkHidden, setIsDisplayMenuLinkHidden] = useState(true);
+
+  const SMALL_SCREEN = 640;
+  const MEDIUM_SCREEN = 768;
 
   const onEnterKeyDownSearchArtist = async ({ key }) => {
     if (key === ENTER) setSearchedArtist(searchedArtistInput)
   }
+
   const clearRecentlySearchedArtists = () => setRecentlySearchedArtists([])
   const clearRecentlyListenedSongs = () => setRecentlyListenedSongs([])
   const clearFavoriteSongs = () => setFavoriteSongs([])
 
-
   const handleClickLogout = () => {
-    clearRecentlySearchedArtists()
-    clearRecentlyListenedSongs()
-    clearFavoriteSongs()
+    clearRecentlySearchedArtists();
+    clearRecentlyListenedSongs();
+    clearFavoriteSongs();
+  }
+
+  const handleOnClickHamburguer = () => {
+    setIsDisplayMenuLinkHidden(!isDisplayMenuLinkHidden)
   }
 
   const handleOnClickOpenSearchInput = () => {
@@ -54,34 +63,60 @@ function SearchAsideBarLinks(props) {
   }, [handleWindowResize])
 
   useEffect(() => {
-    if (windowSize < 768) {
-      setIsSearchMessageVisible(true);
-      setIsSearchedArtistInputVisible(false);
+    const onChangeWindowSizeSetSearchInputDisplay = () => {
+      if (windowSize < MEDIUM_SCREEN) setIsSearchedArtistInputVisible(false);
+      if (windowSize >= MEDIUM_SCREEN) setIsSearchedArtistInputVisible(true)
     }
-    
-    if (windowSize >= 768) {
-      setIsSearchedArtistInputVisible(true)
-      setIsSearchMessageVisible(false);
+    onChangeWindowSizeSetSearchInputDisplay();
+  }, [windowSize])
+
+  useEffect(() => {
+    const onChangeWindowSizeSetSearchMessageDisplay = () => {
+      if (windowSize < MEDIUM_SCREEN) setIsSearchMessageVisible(true);
+      if (windowSize >= MEDIUM_SCREEN) setIsSearchMessageVisible(false);
     }
+    onChangeWindowSizeSetSearchMessageDisplay();
+  }, [windowSize])
+
+  useEffect(() => {
+    const onChangeWindowSizeSetMenuLinkDisplay = () => {
+      if (windowSize < SMALL_SCREEN) setIsDisplayMenuLinkHidden(true);
+      if (windowSize >= SMALL_SCREEN) setIsDisplayMenuLinkHidden(false);
+    }
+    onChangeWindowSizeSetMenuLinkDisplay()
   }, [windowSize])
 
   return (
-    <div className="place-self-center grid grid-cols-5 place-items-center pb-3 md:pb-3 
-      justify-center items-start gap-1 w-full
+    <div className="pb-3 flex flex-col gap-3 justify-center items-start w-full sm:gap-1 
+      sm:grid sm:grid-cols-5 md:pb-3
       md:flex md:flex-col md:gap-3">
+        <div
+          className="flex flex-row items-end justify-start gap-1 group sm:hidden"
+          onClick={ handleOnClickHamburguer }
+        >
+          <HamburguerSVG
+            className="shrink-0 w-7 h-7 stroke-white stroke-2 
+              opacity-50 group-hover:opacity-100 transition duration-500"
+          />
+          <span className="text-white font-semibold opacity-50
+              group-hover:opacity-100 transition duration-500">
+            Menu
+          </span>
+        </div>
         <label
           htmlFor=""
-          className="flex flex-row items-center justify-center gap-1 md:gap-2 md:mb-4"
+          className="group hidden gap-1 sm:flex sm:flex-row sm:gap-2 sm:items-center md:gap-2 md:mb-4"
+          style={{ display: isDisplayMenuLinkHidden ? NONE : FLEX }}
         >
           <MagnifyingGlassSVG
             className="shrink-0 z-20 stroke-2 opacity-50 stroke-white fill-black
-              md::opacity-100 md:fill-white md:stroke-black md:absolute md:left-6"
-              onClick={ handleOnClickOpenSearchInput }
+              md:opacity-100 md:fill-white md:stroke-black md:absolute md:left-6"
+            onClick={ handleOnClickOpenSearchInput }
           />
           <span
             className="block text-white font-semibold opacity-50
               group-hover:opacity-100 transition duration-500 md:hidden"
-            style={{ display: isSearchMessageVisible ? '' : 'none' }}
+            style={{ display: isSearchMessageVisible ? '' : NONE }}
             onClick={ handleOnClickOpenSearchInput }
           >
             Search
@@ -93,10 +128,10 @@ function SearchAsideBarLinks(props) {
               placeholder="Search by artist name"
               onChange={ ({ target: { value }}) => setSearchedArtistInput(value) }
               onKeyDown={ onEnterKeyDownSearchArtist }
-              className="translate-y-[2.5rem] left-0 z-10 absolute
+              className="translate-y-[2.5rem] left-1 z-10 absolute
                 rounded-full p-3 indent-7 w-fit focus:outline-none
                 md:translate-y-0 md:translate-x-0 md:z-0 md:relative"
-              style={{ display: isSearchedArtistInputVisible ? '' : 'none' }}
+              style={{ display: isSearchedArtistInputVisible ? '' : NONE }}
             />
          
         </label>
@@ -104,7 +139,8 @@ function SearchAsideBarLinks(props) {
       
       <Link
         to="/search"
-        className="flex items-center gap-1 sm:gap-2 group"
+        className="group hidden gap-1 sm:flex sm:flex-row sm:gap-2 sm:items-center"
+        style={{ display: isDisplayMenuLinkHidden ? NONE : FLEX }}
       >
         <HomeSVG className="shrink-0 w-7 h-7 stroke-white stroke-2 opacity-50
           group-hover:opacity-100 transition duration-500"/>
@@ -116,7 +152,8 @@ function SearchAsideBarLinks(props) {
 
       <Link
         to="/profile"
-        className="flex items-center gap-1 sm:gap-2 group"
+        className="group hidden gap-1 sm:flex sm:flex-row sm:gap-2 sm:items-center"
+        style={{ display: isDisplayMenuLinkHidden ? NONE : FLEX }}
         >
         <UserSVG className="shrink-0 w-7 h-7 stroke-white stroke-2 opacity-50
           group-hover:opacity-100 transition duration-500"/>
@@ -128,7 +165,8 @@ function SearchAsideBarLinks(props) {
 
       <Link
         to="/favorites"
-        className="flex items-center gap-1 sm:gap-2 group"
+        className="group hidden gap-1 sm:flex sm:flex-row sm:gap-2 sm:items-center"
+        style={{ display: isDisplayMenuLinkHidden ? NONE : FLEX }}
         >
         <StarSVG className="shrink-0 w-7 h-7 stroke-white stroke-2 opacity-50
           group-hover:opacity-100 group-hover:fill-yellow-600 group-hover:stroke-yellow-600 transition duration-500"/>
@@ -140,7 +178,8 @@ function SearchAsideBarLinks(props) {
 
       <Link
         to="/"
-        className="flex items-center gap-1 sm:gap-2 group"
+        className="group hidden gap-1 sm:flex sm:flex-row sm:gap-2 sm:items-center"
+        style={{ display: isDisplayMenuLinkHidden ? NONE : FLEX }}
         onClick={ handleClickLogout }
       >
         <LogoutSVG className="shrink-0 w-7 h-7 stroke-white stroke-2 opacity-50
--- trybe-projects/trybetunes/src/constants/strings.js
+++ trybe-projects/trybetunes/src/constants/strings.js
@@ -2,3 +2,5 @@ export const ENTER = 'Enter';
 export const EXPLICIT = 'explicit';
 export const NOT_EXPLICIT = 'notExplicit';
 export const NO_PARENTAL_ADVISORY = 'No parental advisory';
+export const NONE = 'none';
+export const FLEX = 'flex';
